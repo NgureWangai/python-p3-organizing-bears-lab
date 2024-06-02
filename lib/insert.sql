a4832d0e30bb0459865763ec2edd4b296b7cca23
NgureWangai <ngurewangai@gmail.com>
2024-06-02 21:08:55 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 23, "M", "Chocolate", "Docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 18, "M", "Black", "Ferocious", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 20, "F", "Brown", "Inquisitive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 30, "M", "Black", "Dominant", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 21, "F", "White", "Relaxed", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 17, "M", "Grey", "Social", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("wendy", 24, "F", "Brown", "Timid", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 30, "M", "White", "Submissive", 1);
